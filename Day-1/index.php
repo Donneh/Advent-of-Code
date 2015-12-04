12ebd8ec90f44483bc1d8aefe4f033ccedb94d2a
Donny van Walsem <donnehvw@gmail.com>
2015-12-04 14:25:50 +0100

    Refactored day 1

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,20 +1,27 @@
 <?php
+function floorDirection($floor, $character) {
+    if($character == '(') {
+        return $floor + 1;
+    }
+
+    return $floor - 1;
+}
+
+function floorPosition($input, $floor) {
+    $location = 1;
+
+    for($i = 0; $i < strlen($input); $i++) {
+        $location = floorDirection($location, $input[$i]);
+
+        if($location == $floor) {
+            return $location;
+        }
+    }
+}
+
 $content = file_get_contents("input.txt");
 
 $result = substr_count($content, '(') - substr_count($content, ')');
 
-function floorPosition($input, $floor) {
-    $character = 1;
-    for($i = 0; $i < strlen($input); $i++) {
-        if($input[$i] == '(') {
-            $character += 1;
-        } else {
-            $character -= 1;
-        }
 
-        if($character == $floor) {
-            return $i;
-        }
-    }
-}
 echo floorPosition($content, '-1') . PHP_EOL;
